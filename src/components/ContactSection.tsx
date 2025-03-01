ab2352a6da063df9c3fe2f5d6d2b82d2aadedd64
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-03-01 15:31:28 -0300

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { useLanguage } from '@/contexts/LanguageContext';
+import { translations } from '@/lib/translations';
 import Link from 'next/link';
 
 export default function ContactSection() {
-    return (
-      <section className="py-8 text-center transition-opacity duration-300">
-        <h2 className="text-2xl font-semibold text-fuchsia-800 hover:text-white transition-colors duration-300">
-          Want to add or remove a review? Send me an email
-        </h2>
-        <p className="text-gray-400">
-          Email: <Link href="mailto:lucasbg.lobo@gmail.com">lucasbg.lobo@gmail.com</Link>
-        </p>
-      </section>
-    );
+  const { isEnglish } = useLanguage();
+  const t = translations[isEnglish ? 'en' : 'pt'];
+
+  return (
+    <section className="py-8 text-center transition-opacity duration-300" id='contact'>
+      <h2 className="text-2xl font-semibold text-fuchsia-800 hover:text-white transition-colors duration-300">
+        {t.contact.title}
+      </h2>
+      <p className="text-gray-400">
+        {t.contact.email} <Link href="mailto:lucasbg.lobo@gmail.com">lucasbg.lobo@gmail.com</Link>
+      </p>
+    </section>
+  );
 }
