06065523c091556a58e52c13d661eafa35cf4a87
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-03-01 16:58:32 -0300

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -9,7 +9,7 @@ export default function ContactSection() {
   const t = translations[isEnglish ? 'en' : 'pt'];
 
   return (
-    <section className="py-8 text-center transition-opacity duration-300" id='contact'>
+    <section className="py-8 text-center transition-opacity duration-300">
       <h2 className="text-2xl font-semibold text-fuchsia-800 hover:text-white transition-colors duration-300">
         {t.contact.title}
       </h2>
