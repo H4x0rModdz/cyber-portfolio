8c827fd55f77c62f7c5aa2d573a0aebbe4e3d9f7
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-03-01 15:30:22 -0300

    Update IProject.ts

--- src/types/IProject.ts
+++ src/types/IProject.ts
@@ -1,10 +1,10 @@
 export interface IProject {
-    name: string;
-    videoPath: string;
-    description: string;
-    image: string;
-    stacks: string[];
-    createdAt: string;
-    repositoryPath?: string;
-    tag?: string;
-  }
+  name: string;
+  videoPath: string;
+  description: string;
+  image: string;
+  stacks: string[];
+  createdAt: string;
+  repositoryPath?: string;
+  tag?: string;
+}
